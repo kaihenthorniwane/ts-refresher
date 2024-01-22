35750220921f79f82e69b884be2a87127abc5c50
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-01-22 02:09:26 -0800

    Initial commit

---
+++ first-app.ts
@@ -0,0 +1,31 @@
+let userName: string = "Max";
+let userAge: number = 24;
+let isValid: boolean = true;
+let multiType: string | boolean = true;
+let objVariable: {
+  stringProp: string;
+  booleanProp: boolean;
+  numberProp: number;
+  multiProp: string | number;
+} = {
+  stringProp: "val",
+  booleanProp: true,
+  numberProp: 12,
+  multiProp: "string",
+};
+let arrayVariable: string[] = ["hi", "yes", "no"];
+let arrayOfObjects: { numProp: number; boolProp: boolean }[] = [
+  { numProp: 1, boolProp: true },
+  { numProp: 2, boolProp: false },
+];
+
+function voidFunction(paramOne: number, paramTwo: boolean): void {
+  const result =
+    "The number is: " + paramOne + ", and the boolean is: " + paramTwo;
+  console.log(result);
+}
+
+function returningFunction(paramOne: number, paramTwo: number): boolean {
+  //check if paramOne is greater than paramTwo
+  return paramOne > paramTwo;
+}
